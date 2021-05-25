335062a29b3148fe6e5d490f50099376eb100e61
Michail Koskinas <michailkoskinas@Michails-MacBook-Pro.local>
2021-05-25 14:51:21 +0200

    download csv file

---
+++ helpers.py
@@ -0,0 +1,30 @@
+## Download the csv file from google cloud storage
+
+from google.cloud import storage
+from dotenv import load_dotenv
+import pandas as pd  
+
+load_dotenv()
+
+def load_from_gcs(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_blob_name = "storage-object-name"
+    # destination_file_name = "local/path/to/file"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+
+    # Construct a client side representation of a blob.
+    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve any content from Google Cloud Storage. As we don't need additional data,
+    # using `Bucket.blob` is preferred here.
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+    print(
+        "Blob {} downloaded to {}.".format(
+            source_blob_name, destination_file_name
+        )
+    )
+    reco_data = pd.read_csv(destination_file_name)
+    return reco_data
